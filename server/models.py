d432e7dd5530674898469cfe8af03ad746108662
linda-20 <lindaaron40@gmail.com>
2024-04-19 18:42:45 +0300

    feat:Completed code challenge

--- server/models.py
+++ server/models.py
@@ -1,7 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
-from sqlalchemy.orm import validates
-from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy import MetaData, ForeignKey
+from sqlalchemy.orm import relationship, validates
 from sqlalchemy_serializer import SerializerMixin
 
 metadata = MetaData(naming_convention={
@@ -12,44 +11,48 @@ db = SQLAlchemy(metadata=metadata)
 
 
 class Sweet(db.Model, SerializerMixin):
-    __tablename__ = 'sweets'
+    __tablename__ = 'sweets'  
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
 
-    # Add relationship
-    
-    # Add serialization
-    
-    def __repr__(self):
+    vendor_sweets = relationship('VendorSweet', back_populates='sweet')
+
+    def _repr_(self):
         return f'<Sweet {self.id}>'
 
 
 class Vendor(db.Model, SerializerMixin):
-    __tablename__ = 'vendors'
+    __tablename__ = 'vendors'  # Corrected typo
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
 
-    # Add relationship
-    
-    # Add serialization
-    
-    def __repr__(self):
+    vendor_sweets = relationship('VendorSweet', back_populates='vendor')
+
+    def _repr_(self):
         return f'<Vendor {self.id}>'
 
 
 class VendorSweet(db.Model, SerializerMixin):
-    __tablename__ = 'vendor_sweets'
+    __tablename__ = 'vendor_sweets'  
 
     id = db.Column(db.Integer, primary_key=True)
     price = db.Column(db.Integer, nullable=False)
 
-    # Add relationships
-    
-    # Add serialization
-    
-    # Add validation
-    
-    def __repr__(self):
+    sweet_id = db.Column(db.Integer, ForeignKey('sweets.id'), nullable=False)
+    vendor_id = db.Column(db.Integer, ForeignKey('vendors.id'), nullable=False)
+
+    sweet = relationship('Sweet', back_populates='vendor_sweets')
+    vendor = relationship('Vendor', back_populates='vendor_sweets')
+
+    @validates('price')
+    def validate_price(self, key, price):
+        if price is None:
+            raise ValueError('Price must have a value.')
+        if price < 0:
+            raise ValueError('Price must be non-negative.')
+        return price
+
+    def _repr_(self):
         return f'<VendorSweet {self.id}>'
